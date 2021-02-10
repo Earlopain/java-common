4cf90adf43434c5eb9cec422690b5f334494aa43
Earlopain <earlopain@c5h8no4na.net>
2021-02-10 21:12:43 +0100

    Make ApiClients take URI insteand of String

--- src/main/java/net/c5h8no4na/common/network/ApiClient.java
+++ src/main/java/net/c5h8no4na/common/network/ApiClient.java
@@ -1,5 +1,6 @@
 package net.c5h8no4na.common.network;
 
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest.Builder;
 import java.util.Base64;
@@ -14,7 +15,7 @@ public abstract class ApiClient<T> {
 
 	protected abstract Builder getBuilderBase();
 
-	public abstract NetworkRequest<T> get(String url);
+	public abstract NetworkRequest<T> get(URI url);
 
 	public static String basicAuth(String username, String password) {
 		return "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
