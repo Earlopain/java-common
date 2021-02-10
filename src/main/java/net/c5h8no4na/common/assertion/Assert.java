9bbac47961b9beb0e8415de66a4d3a0f1aa0e3fd
Earlopain <earlopain@c5h8no4na.net>
2021-02-10 12:46:23 +0100

    Initial commit

---
+++ src/main/java/net/c5h8no4na/common/assertion/Assert.java
@@ -0,0 +1,44 @@
+package net.c5h8no4na.common.assertion;
+
+public class Assert {
+
+	public static void isTrue(Boolean value, String message) {
+		if (value != true) {
+			throw new AssertionError(message);
+		}
+	}
+
+	public static void isTrue(Boolean value) {
+		isTrue(value, String.format("%b passed, was expected to be true", value));
+	}
+
+	public static void isFalse(Boolean value, String message) {
+		if (value != false) {
+			throw new AssertionError(message);
+		}
+	}
+
+	public static void isFalse(Boolean value) {
+		isFalse(value, String.format("%b was passed, was expected to be false", value));
+	}
+
+	public static void notNull(Object o, String message) {
+		if (o == null) {
+			throw new AssertionError(message);
+		}
+	}
+
+	public static void notNull(Object o) {
+		notNull(o, "Passed object was expected to not be null");
+	}
+
+	public static void isNull(Object o, String message) {
+		if (o != null) {
+			throw new AssertionError(message);
+		}
+	}
+
+	public static void isNull(Object o) {
+		isNull(o, "Passed object was expected to be null");
+	}
+}
